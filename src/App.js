663cb5d609e3b070ab6955ef5d954ba3f48623d2
alex <lapkesa@gmail.com>
2022-12-10 15:28:25 +0200

    new:add new components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
-import './App.css';
+import Categories from './components/Categories';
+import Header from './components/Header';
+import PizzaBlock from './components/PizzaBlock';
+import Sort from './components/Sort';
+import './scss/app.scss';
 
 function App() {
-  return <div className="App">WOOOOW</div>;
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaBlock title="Cheese" price={20} />
+            <PizzaBlock title="Cheese" price={20} />
+            <PizzaBlock title="Cheese" price={20} />
+            <PizzaBlock title="Cheese" price={20} />
+            <PizzaBlock title="Cheese" price={20} />
+            <PizzaBlock title="Cheese" price={20} />
+            <PizzaBlock title="Cheese" price={20} />
+            <PizzaBlock title="Cheese" price={20} />
+            <PizzaBlock title="Cheese" price={20} />
+            <PizzaBlock title="Cheese" price={20} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
