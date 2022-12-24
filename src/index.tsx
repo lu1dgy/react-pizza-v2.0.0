1f15092a0704d2e79d8c5f797a9dfeb742a2dae1
alex <lapkesa@gmail.com>
2022-12-24 21:12:03 +0200

    new:add tsconfig

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import App from './App';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <BrowserRouter>
     <Provider store={store}>
